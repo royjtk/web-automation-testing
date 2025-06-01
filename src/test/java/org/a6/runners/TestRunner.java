b54fded4e5508e8f68793f8c91031b9753cbf072
Muhammad Azhar <117720953+MuhammadAzhar9@users.noreply.github.com>
2025-06-02 00:32:54 +0700

    Integrate Allure reporting for test results; update dependencies and configuration

--- src/test/java/org/a6/runners/TestRunner.java
+++ src/test/java/org/a6/runners/TestRunner.java
@@ -5,19 +5,17 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features") // This should match the folder in src/test/resources
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.a6.stepdefs")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports/cucumber-pretty.html, json:target/cucumber.json, junit:target/cucumber-reports/cucumber-results.xml, rerun:target/failed_scenarios.txt")
+@SelectClasspathResource("features") // Folder feature di src/test/resources/features
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.a6.stepdefs") // sesuaikan dengan package step defs kamu
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@login or @logout or @dashboard")
 @ConfigurationParameter(key = "cucumber.junit-platform.naming-strategy", value = "long")
 @ConfigurationParameter(key = "cucumber.execution.dry-run", value = "false")
 @ConfigurationParameter(key = "cucumber.execution.monochrome", value = "true")
 public class TestRunner {
-    // The class body remains empty
+    // kosong, tidak perlu method apapun
 }
